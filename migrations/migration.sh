0642eb0dc035c9b33338216cc8e34c9aabac74d1
JGO22390 <jules.goregues_carmona@open-groupe.com>
2025-10-10 18:21:53 +0200

    fix(migration): RUDI-6015: correction lancement en ligne de commande runMigration.js

--- migrations/migration.sh
+++ migrations/migration.sh
@@ -1,3 +1,4 @@
 #! ./bin/bash
+# shellcheck shell=bash
 
 node ./migrations/runMigration.js --conf ../rudi-node-local/conf/rudi-catalog-conf.ini --profiles ../rudi-node-local/conf/rudi-catalog-profiles.ini --portal_conf ../rudi-node-local/conf/rudi-catalog-portal.ini
