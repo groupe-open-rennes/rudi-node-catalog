859bd96c5b33e1dc16a18cf0f33bb2060081a08a
JGO22390 <jules.goregues_carmona@open-groupe.com>
2025-10-13 17:17:53 +0200

    fix(migration): RUDI-6015: suppression

--- migrations/scripts/002__migration_bidon1.js
+++
@@ -1,9 +0,0 @@
-import { logD } from '../../src/utils/logging.js'
-
-const mod = 'migration_bidon1'
-
-export async function migrate({ connection }) {
-  const fun = 'migrate'
-
-  logD(mod, fun, 'migrate')
-}
--- migrations/scripts/005__migration_bidon3.js
+++
@@ -1,9 +0,0 @@
-import { logD } from '../../src/utils/logging.js'
-
-const mod = 'migration_bidon3'
-
-export async function migrate({ connection }) {
-  const fun = 'migrate'
-
-  logD(mod, fun, 'migrate')
-}
